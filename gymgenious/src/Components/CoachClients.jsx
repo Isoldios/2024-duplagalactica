dca8f508f77ad38af06ad40475b6988e1ea219b6
Isoldios <isoldi772@gmail.com>
2025-02-14 01:15:08 -0300

    correcting commit

--- gymgenious/src/Components/CoachClients.jsx
+++ gymgenious/src/Components/CoachClients.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import NewLeftBar from '../real_components/NewLeftBar'
 import { useNavigate } from 'react-router-dom';
 import Backdrop from '@mui/material/Backdrop';
@@ -11,6 +11,12 @@ import CustomTable from '../real_components/Table3columns.jsx';
 import verifyToken from '../fetchs/verifyToken.jsx'
 import fetchUser from '../fetchs/fetchUser.jsx'
 import fetchAssistance from '../fetchs/fetchAssistance.jsx';
+import QrCodeIcon from '@mui/icons-material/QrCode';
+import Button from '@mui/material/Button';
+import { QRCodeCanvas } from "qrcode.react";
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 function CouchClasses() {
   const [userMail,setUserMail] = useState(null)
@@ -20,6 +26,16 @@ function CouchClasses() {
   const [errorToken,setErrorToken] = useState(false);
   const [type, setType] = useState(null);
   const [newRows, setNewRows] = useState([]);
+  const [openQr, setOpenQr] = useState(false);
+  const isSmallScreen700 = useMediaQuery('(max-width:700px)');
+  
+  const handleOpenQr = () => {
+    setOpenQr(true);
+  };
+
+  const handleCloseQr = () => {
+    setOpenQr(false);
+  };
 
   useEffect(() => {
     if (type!='coach' && type!=null) {
@@ -53,6 +69,43 @@ function CouchClasses() {
     return
   }
 
+  const EventQRCode = () => {
+
+    return (
+      <div className="vh-100" style={{position:'fixed',zIndex:1000,display:'flex',flex:1,width:'100%',height:'100%',opacity: 1,
+        visibility: 'visible',backgroundColor: 'rgba(0, 0, 0, 0.5)'}} onClick={handleCloseQr}>
+          <MDBContainer style={{display:'flex'}}>
+            <MDBRow className="justify-content-center" onClick={(e) => e.stopPropagation()} style={{flex:1,display:'flex',alignContent:'center'}}>
+              <MDBCol md="9" lg="7" xl="5" className="mt-5" style={{width:'40%'}}>
+                <MDBCard style={{ borderRadius: '15px', backgroundColor: '#F5F5F5' }}>
+                  <MDBCardBody className="p-4 text-black">
+                    <div>
+                      <MDBTypography tag='h6' style={{color: '#424242',fontWeight:'bold' }}>Assistance for </MDBTypography>
+                    </div>
+                    <div style={{justifyContent:'center',left:'23%',alignContent:'center',width:'60%',position:'relative'}}>
+                    <QRCodeCanvas value={`http://localhost:3000/mark-attendance?token=${qrToken}`} size={256} />
+                    </div>
+                    <button 
+                        onClick={handleCloseQr}
+                        className="custom-button-go-back-managing"
+                        style={{
+                          zIndex: '2',
+                          position: 'absolute', 
+                          top: '1%',
+                          left: isSmallScreen700 ? '88%' : '90%',
+                        }}
+                      >
+                        <CloseIcon sx={{ color: '#F5F5F5' }} />
+                      </button>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+      </div>
+    );
+  };
+
 
   return (
     <div className="App">
@@ -73,6 +126,32 @@ function CouchClasses() {
         )}
           <>
         <NewLeftBar/>
+        <div className='input-container-buttons' style={{left: '7vh', position: 'absolute', top: '0.5%'}}>
+          <div className='input-small-container-buttons'>
+            <Button
+              style={{
+                  backgroundColor: '#48CFCB',
+                  borderRadius: '50%',
+                  width: '5vh',
+                  height: '5vh',
+                  minWidth: '0',
+                  minHeight: '0',
+                  padding: '0',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+              }}
+              onClick={handleOpenQr}
+              >
+              <QrCodeIcon sx={{ color: 'light green' }} />
+            </Button>
+          </div>
+        </div>
+        {openQr && (
+            <div className="Modal" style={{zIndex:'1001'}}>
+            <EventQRCode/>
+            </div>
+        )}
         {openCircularProgress ? (
             <Backdrop open={openCircularProgress} sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}>
                 <Loader></Loader>
